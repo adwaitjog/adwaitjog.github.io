aa4d3e59aa9cd5315af63ef7cbb430262fe3e5a9
Adwait Jog <adwaitjog@gmail.com>
2016-10-12 14:42:15 -0400

    new website

--- jemdoc.py
+++ jemdoc.py
@@ -683,7 +683,7 @@ def br(b, f, tableblock=False):
   b = re.sub(r, r'<b>\1</b>', b)
   
   # Deal with *underline*.
-  r = re.compile(r'(?<!\\)\_(.*?)(?<!\\)\_', re.M + re.S)
+  r = re.compile(r'(?<!\\)\%(.*?)(?<!\\)\%', re.M + re.S)
   b = re.sub(r, r'<u>\1</u>', b)
 
   # Deal with +monospace+.
